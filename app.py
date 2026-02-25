28b7c8a5978b50d6a928fbc308e3925c1204aa22
Hasan Mohammad <hasan.mohammad@v2.ltd>
2026-02-25 19:07:48 +0600

    added temple subject

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def init_session_state():
         'email_results': [],
         'file_errors': [],
         'email_template': TemplateManager.get_default_template(),
-        'email_subject': 'Invitation to Online Assessment | TAM – Digital Banking | 26 February',
+        'email_subject': 'Assessment Link & Login Credentials | TAM – Digital Banking | 26 February',
         'program_id': int(Config.DEFAULT_PROGRAM_ID) if Config.DEFAULT_PROGRAM_ID else 1,
         'round_id': int(Config.DEFAULT_ROUND_ID) if Config.DEFAULT_ROUND_ID else 1,
         'session_time': Config.DEFAULT_SESSION_TIME or '730h',
@@ -613,7 +613,7 @@ with tab4:
                 st.session_state.email_subject = 'Invitation to Online Assessment | TAM – Digital Banking | 26 February'
             else:
                 st.session_state.email_template = TemplateManager.get_default_template()
-                st.session_state.email_subject = 'Invitation to Online Assessment | TAM – Digital Banking | 26 February'
+                st.session_state.email_subject = 'Assessment Link & Login Credentials | TAM – Digital Banking | 26 February'
             st.rerun()
 
     with col_preview:
